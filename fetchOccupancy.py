9dfc5b466426b0c874e7092c7339e794e1cbb2b9
Etienne Mathier <etienne@mathier.ch>
2026-06-03 14:07:22 +0200

    Set up logging

--- fetchOccupancy.py
+++ fetchOccupancy.py
@@ -1,35 +1,30 @@
-import marimo
-
-__generated_with = "0.23.8"
-app = marimo.App(width="medium")
-
-
-@app.cell
-def _():
-    import marimo as mo
-    import polars as pl
-    from google.cloud import bigquery
-
-    # 1. Explicitly state your project ID (where the query runs)
-    PROJECT_ID = "optiswim-scraper" 
-
-    # 2. Initialize the client (ADC handles the auth automatically!)
-    client = bigquery.Client(project=PROJECT_ID)
-
-    # 3. Perform a query
-    QUERY = (
-        'SELECT timestamp,hallenbad_city FROM `badi_data.currentfill` '
-        'ORDER BY timestamp DESC'
-    )
+import polars as pl
+from google.cloud import bigquery
+from logging_config import get_logger
+log = get_logger(__name__)
 
 
-    query_job = client.query(QUERY)  # API request
-    rows = query_job.result()  # Waits for query to finish
-
-    df = pl.from_arrow(rows.to_arrow())
-    df.write_parquet("city-occupancy.parquet")
-    return
+log.info("Starting to fetch occupancy")
+bq_client = bigquery.Client(project="optiswim-scraper")
 
 
-if __name__ == "__main__":
-    app.run()
+"""
+
+
+
+# SQL
+QUERY = (
+    'SELECT * FROM `badi_data.currentfill` '
+    'ORDER BY timestamp DESC'
+)
+
+# Diagnostics
+query_job = bq_client.query(QUERY)  
+rows = query_job.result()  
+
+df = pl.from_arrow(rows.to_arrow())
+df.write_parquet("city-occupancy.parquet")
+
+"""
+
+
