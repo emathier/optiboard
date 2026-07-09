f97ce5045ecde01d16cabf3c453a05f084d842eb
Etienne Mathier <etienne@mathier.ch>
2026-07-10 00:42:18 +0200

    fix: remove unsafe_allow_javascript from Google Analytics component injection

--- streamlit/Predictions.py
+++ streamlit/Predictions.py
@@ -26,7 +26,7 @@ st.set_page_config(
 )
 
 # Inject the script into the app (hidden from view)
-components.html(ga_html, height=0, width=0, unsafe_allow_javascript=True)
+components.html(ga_html, height=0, width=0)
 
 # Custom CSS to remove top whitespace and make layout compact
 st.markdown("""
